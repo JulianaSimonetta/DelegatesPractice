694e259b34e390136c820044ffe36a0dfe05c07d
Juliana Simonetta <jsimonetta14@outlook.com>
2023-10-11 22:37:30 -0300

    Initial Commit

---
+++ delegatePracticas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegatePracticas
+//
+//  Created by juliana simonetta on 11/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
