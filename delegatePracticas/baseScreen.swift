f4c44a1f49fcf473483bdc48fe22b278addcb2f4
Juliana Simonetta <jsimonetta14@outlook.com>
2023-10-18 18:31:47 -0300

    functionality working great

--- delegatePracticas/baseScreen.swift
+++ delegatePracticas/baseScreen.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class baseScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ delegatePracticas/selectionScreen.swift
@@ -0,0 +1,18 @@
+//
+//  selectionScreen.swift
+//  delegatePracticas
+//
+//  Created by juliana simonetta on 11/10/2023.
+//
+
+import Foundation
+import UIKit
+
+class selectionScene: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
